5684c2859e415b404c3f0b0d4e6f40195adcd686
JplsMura <joaopedrolima.s@hotmail.com>
2024-02-05 15:51:44 -0300

    Creating logout and recovering the logged in user

--- app/Http/Controllers/Api/Auth/AuthApiController.php
+++ app/Http/Controllers/Api/Auth/AuthApiController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers\Api\Auth;
 
-use App\Http\Controllers\Controller;
-
 use App\Http\Requests\Api\Auth\{
     AuthApiRequest
 };
@@ -11,7 +9,14 @@ use App\Http\Requests\Api\Auth\{
 use App\Repositories\{
     UserRepository
 };
-use Illuminate\Support\Facades\Hash;
+
+use Illuminate\Support\Facades\{
+    Auth,
+    Hash
+};
+
+use Illuminate\Http\Response;
+use App\Http\Controllers\Controller;
 use Illuminate\Validation\ValidationException;
 
 class AuthApiController extends Controller
@@ -26,12 +31,23 @@ class AuthApiController extends Controller
         $user = $this->userRepository->findByEmail($request->email);
      
         if (! $user || ! Hash::check($request->password, $user->password)) {
-            throw ValidationException::withMessages([
-                'email' => ['The provided credentials are incorrect.'],
-            ]);
+            throw ValidationException::withMessages(['email' => ['The provided credentials are incorrect.']]);
         }
      
+        // Autênciação de um token por usuário somente
+        $user->tokens()->delete();
         $token = $user->createToken($request->device_name)->plainTextToken;
         return response()->json(['token' => $token]);
+    }
+
+    public function logout()
+    {
+        Auth::user()->tokens()->delete();
+        return response()->json(['message' => 'Tokens Revoked'], Response::HTTP_NO_CONTENT);
+    }
+
+    public function me()
+    {
+        return response()->json(['user' => Auth::user()]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,10 @@ use App\Http\Controllers\Api\Auth\{
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/me', [AuthApiController::class, 'me'])->name('me');
+    Route::post('/logout', [AuthApiController::class, 'logout'])->name('logout');
+});
 
 Route::apiResource('/permissions', PermissionController::class);
 
@@ -20,5 +23,7 @@ Route::put('/users/{user}', [UserController::class, 'update'])->name('users.upda
 Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth');
 
 Route::get('/', fn () => response()->json(['message' => 'Hello World!']));
