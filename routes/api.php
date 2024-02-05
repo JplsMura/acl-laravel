d9026250ec0c457ec2b5ed50629d10d894aed303
JplsMura <joaopedrolima.s@hotmail.com>
2024-02-05 16:00:50 -0300

    Grouping user routes

--- routes/api.php
+++ routes/api.php
@@ -11,18 +11,14 @@ use App\Http\Controllers\Api\Auth\{
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/me', [AuthApiController::class, 'me'])->name('me');
     Route::post('/logout', [AuthApiController::class, 'logout'])->name('logout');
+
+    Route::apiResource('/permissions', PermissionController::class);
+
+    Route::apiResource('/users', UserController::class);
 });
-
-Route::apiResource('/permissions', PermissionController::class);
-
-Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
 
 Route::post('/auth', [AuthApiController::class, 'auth'])->name('auth');
 
